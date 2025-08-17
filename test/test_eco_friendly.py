bf74fdf0e66a00ad109c97af9f2271f3ce7c5ca9
Andrey Rassokhin <ambruciy@gmail.com>
2025-08-18 00:58:53 +0300

    Add tests

---
+++ test/test_eco_friendly.py
@@ -0,0 +1,17 @@
+def test_all_el(eco_friendly):
+    eco_friendly.open_page()
+    eco_friendly.accept_cookie()
+    eco_friendly.check_number_of_products_on_a_page()
+
+
+def test_open_card_product(eco_friendly, product_card):
+    eco_friendly.open_page()
+    eco_friendly.accept_cookie()
+    name_product = eco_friendly.open_card_product()
+    product_card.check_product_card(name_product=name_product)
+
+
+def test_sorted_by_price(eco_friendly):
+    eco_friendly.open_page()
+    eco_friendly.accept_cookie()
+    eco_friendly.check_sort_by_price()
